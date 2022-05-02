7fb3578ff631699954c760b6ec96606c96b06e69
Igor Gregori <igsilvacarlos@gmail.com>
2022-05-02 03:11:51 -0300

    feat: add logical of conversational flow

--- message-processor.js
+++ message-processor.js
@@ -1,19 +1,27 @@
 import {
-  servicesOffered
+  servicesOffered,
+  scheduleConsultation,
+  rescheduleConsultation,
+  pendingInstallmentInquiries,
+  requestAttest,
+  redirectToAttendant
 } from './constant-messages.js'
 
 export function handleMessage(choice, lastTemplateId) {
-  console.log('choice =>', typeof choice)
+  console.log('🔵 lastTemplateId =>', lastTemplateId)
+  console.log('🟠 choice =>', choice)
 
-  if (lastTemplateId === 0) {
-    console.log('entrou aq')
-    if (choice === '1') {
-      return servicesOffered
-    }
+  if (lastTemplateId === 1) {
+    if (choice === 1) return servicesOffered
+    if (choice === 2) return scheduleConsultation
+    if (choice === 3) return rescheduleConsultation
+    if (choice === 4) return pendingInstallmentInquiries
+    if (choice === 5) return requestAttest
+    if (choice === 6) return redirectToAttendant
   }
 
   if (lastTemplateId === 2) {
-
+    if (choice === 1) return scheduleConsultation
   }
 
   throw new Error('flow error')
